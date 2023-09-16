f3e05f2b3399413f4e815e4c689c06265d807d9c
山城潇洒哥 <81630220@qq.com>
2023-09-16 14:52:01 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '0b098b49-f68f-4f52-af18-356afcce5dde';
 
-let proxyIP = '';
+let proxyIP = 'sp.kjkkk.link';
 
 
 if (!isValidUUID(userID)) {
